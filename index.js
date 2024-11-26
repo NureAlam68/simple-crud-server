7a24a82e44585c1ccf9ab106a0f044619db02f4a
NureAlam68 <nurealam151068@gmail.com>
2024-11-26 16:31:31 +0600

    creat operation

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 const express = require('express');
 const cors = require('cors')
-require('dotenv').config();
 const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
 const port = process.env.PORT || 5000;
 const app = express();
 
 // middleware
 app.use(cors())
 app.use(express.json())
-
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.8kdu5.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
@@ -27,22 +26,19 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
+    // server to mongodb
     const database = client.db("usersDB");
     const userCollection = database.collection("users");
-
-    app.get('/users', async(req, res) => {
-      const cursor = userCollection.find();
-      const result = await cursor.toArray();
-      res.send(result);
-    })
 
     app.post('/users', async(req, res) => {
       const user = req.body;
       console.log('new user', user)
 
-      const result = await userCollection.insertOne(user);
+      const result = await userCollection.insertOne(user)
       res.send(result)
     })
+
+
 
 
 
